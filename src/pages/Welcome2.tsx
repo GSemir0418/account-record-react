ed7deddcb23ef9b052659fb402d47cb76af52dfe
gsemir0418 <845217811@qq.com>
2023-02-21 11:03:58 +0000

    css in js - styled components

--- src/pages/Welcome2.tsx
+++ src/pages/Welcome2.tsx
@@ -1,5 +1,32 @@
-import { NavLink } from 'react-router-dom'
+import { useState } from 'react'
+import styled from 'styled-components'
 
+const BlueBox = styled.div`
+  background-color: blue;
+  color: red;
+  // 支持 scss
+  &:hover {
+    background-color: aliceblue;
+  }
+`
+// 复用组件样式
+const BlueBoxWithWhiteText = styled(BlueBox)`
+  color: white;
+`
+// 接收参数
+const Button = styled.button`
+  color: ${props => props.color};
+`
 export const Welcome2 = () => {
-  return <div>2 <NavLink to='/welcome/3'>next</NavLink></div>
+  const [color, setColor] = useState<'red' | 'green'>('red')
+  const onClick = () => {
+    setColor(color === 'red' ? 'green' : 'red')
+  }
+  return (
+    <div>
+      <BlueBox style={{ border: '10px solid red' }}>2</BlueBox>
+      <BlueBoxWithWhiteText>22</BlueBoxWithWhiteText>
+      <Button onClick={onClick} color={color}>change my color</Button>
+    </div>
+  )
 }
