753153b16068888184f24b6fc9fbf28d0ba6eec3
gsemir0418 <845217811@qq.com>
2023-02-22 02:41:02 +0000

    css in js-unocss

--- src/pages/Welcome3.tsx
+++ src/pages/Welcome3.tsx
@@ -1,5 +1,9 @@
-import { NavLink } from 'react-router-dom'
-
 export const Welcome3 = () => {
-  return <div>3 <NavLink to='/welcome/4'>next</NavLink></div>
+  return (
+  <div shadow-2xl flex justify-center items-center after='content-[hi]'>
+    <div bg-red hover:bg-blue w-200px h-100px>sider</div>
+    <div bg-green flex-1 h-100px>main</div>
+    <div bg-yellow w-100px h-100px>sider</div>
+  </div>
+  )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
+import Unocss from 'unocss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [Unocss(), react()],
 })
